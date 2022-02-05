16cd6f4388ffbe144e75b22be118495f9504680a
koutaro20 <koutarodocomo@outlook.jp>
2022-02-05 22:29:07 +0900

    create class components and functionnal components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from "react";
-import { Component } from "react/cjs/react.production.min";
+// import { Component } from "react/cjs/react.production.min";
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked.")}} />
-      </React.Fragment>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+
+  );
+};
+
+const Cat = () => {
+  return <div>meow!</div>
+};
 
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
